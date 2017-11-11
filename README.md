0f060dab0caa7ff6e7f33b7ef92d6e7485834c95
nunun <hoge.life@gmail.com>
2017-11-11 17:55:55 +0900

    update README

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 
+### Install global volume as nfs server
+
 ````
-curl -sSL https://raw.githubusercontent.com/nunun/docker-volume-global/master/install.sh | sudo sh
+curl -sSL https://raw.githubusercontent.com/nunun/docker-volume-global/master/install.sh | sudo sh -s server <server-ip>
 ````
+
+### Install global volume as nfs client
+
+````
+curl -sSL https://raw.githubusercontent.com/nunun/docker-volume-global/master/install.sh | sudo sh -s <server-ip>
+````
+
--- install.sh
+++ install.sh
@@ -4,60 +4,52 @@ EXPORTS_CONFIG_FILE="/etc/exports"
 EXPORTS_CONFIG_BACKUP_FILE="/etc/exports.bak"
 NETSHARE_DEB_URL="https://github.com/ContainX/docker-volume-netshare/releases/download/v0.17/docker-volume-netshare_0.17_amd64.deb"
 
+# arguments
+if [ "${1}" = "server" ]; then
+        ROLE="server"
+        IP="${2}"
+else
+        ROLE="client"
+        IP="${1}"
+fi
+
+# usage
+if [ -z "${IP}" ]; then
+        echo "usage:"
+        echo " ${0} [server] <server-ip>"
+        exit 1
+fi
+
 # enable error stop
 set -e
 
 # install requirements
 apt-get install wget
 
-# list nodes
-NODES=`docker node ls --format "{{.Hostname}}"`
-if [ -z "${NODES}" ]; then
-        echo "docker command error, or no nodes in swarm."
-        exit 1
-fi
-echo "nodes in swarm:"
-echo "${NODES}"
-echo ""
-
-# node addresses
-ADDRS=`docker inspect ${NODES} --format "{{.Status.Addr}}"`
-if [ -z "${ADDRS}" ]; then
-        echo "docker command error, or could not get ip addresses."
-        exit 1
-fi
-echo "node ip addresses in swarm:"
-echo "${ADDRS}"
-echo ""
-
-# manager node ip address
-MANAGER_ADDR=""
-for n in ${NODES}; do
-        IS_MANAGER_NODE=`docker node inspect ${n} --format "{{.ManagerStatus.Leader}}"`
-        if [ "${IS_MANAGER_NODE}" = "true" ]; then
-                MANAGER_ADDR=`docker node inspect ${n} --format "{{.Status.Addr}}"`
-                break
-        fi
-done
-if [ -z "${MANAGER_ADDR}" ]; then
-        echo "swarm manager did not found."
-        exit 1
-fi
-echo "manager node ip address: ${MANAGER_ADDR}"
-echo ""
-
-# is manger?
-IS_MANAGER=`docker node inspect self --format "{{.ManagerStatus.Leader}}"`
-echo "is manager: ${IS_MANAGER}"
-echo ""
-
-###############################################################################
-###############################################################################
-###############################################################################
 # install nfs server (if manager)
-if [ "${IS_MANAGER}" = "true" ]; then
+if [ "${ROLE}" = "server" ]; then
         echo ""
         echo "[install nfs server]"
+
+        # list nodes
+        NODES=`docker node ls --format "{{.Hostname}}"`
+        if [ -z "${NODES}" ]; then
+                echo "node is not docker manager, or no nodes in swarm."
+                exit 1
+        fi
+        echo "nodes in swarm:"
+        echo "${NODES}"
+        echo ""
+
+        # list node addresses
+        ADDRS=`docker inspect ${NODES} --format "{{.Status.Addr}}"`
+        if [ -z "${ADDRS}" ]; then
+                echo "node is not docker manager, or could not get ip addresses."
+                exit 1
+        fi
+        echo "node addresses:"
+        echo "${ADDRS}"
+        echo ""
 
         # create nfs exports
         EXPORTS=""
@@ -84,39 +76,37 @@ if [ "${IS_MANAGER}" = "true" ]; then
         /etc/init.d/nfs-kernel-server restart
 fi
 
-###############################################################################
-###############################################################################
-###############################################################################
 # install nfs volume
-echo ""
-echo "[install nfs client]"
+if [ "${ROLE}" = "server" -o "${ROLE}" = "client" ]; then
+        echo ""
+        echo "[install nfs client]"
 
-# netshare
-wget -O /tmp/netshare.deb "${NETSHARE_DEB_URL}"
-dpkg -i /tmp/netshare.deb
-service docker-volume-netshare start
-if [ -f "/tmp/netshare.deb" ]; then
-        rm -f /tmp/netshare.deb
+        # netshare
+        wget -O /tmp/netshare.deb "${NETSHARE_DEB_URL}"
+        dpkg -i /tmp/netshare.deb
+        service docker-volume-netshare start
+        if [ -f "/tmp/netshare.deb" ]; then
+                rm -f /tmp/netshare.deb
+        fi
+
+        # remove global volume
+        docker volume inspect ${VOLUME_NAME} && docker volume rm ${VOLUME_NAME}
+        docker volume inspect ${VOLUME_NAME} && echo "could not delete volume '${VOLUME_NAME}'?" && exit 1
+        echo "volume '${VOLUME_NAME}' seems removed or not found."
+        echo "creating volume '${VOLUME_NAME}' ..."
+
+        # create global volume
+        docker volume create \
+           --driver local \
+           --opt type=nfs4 \
+           --opt o=addr=${IP},rw,hard,intr \
+           --opt device=:${EXPORTS_DIR} \
+           ${VOLUME_NAME}
+
+        # inspect global volume
+        docker volume inspect ${VOLUME_NAME}
 fi
-
-# remove global volume
-docker volume inspect ${VOLUME_NAME} && docker volume rm ${VOLUME_NAME}
-docker volume inspect ${VOLUME_NAME} && echo "could not delete volume '${VOLUME_NAME}'?" && exit 1
-echo "volume '${VOLUME_NAME}' seems removed or not found."
-echo "creating volume '${VOLUME_NAME}' ..."
-
-# create global volume
-docker volume create \
-   --driver local \
-   --opt type=nfs4 \
-   --opt o=addr=${MANAGER_ADDR},rw,hard,intr \
-   --opt device=:${EXPORTS_DIR} \
-   ${VOLUME_NAME}
-
-# inspect global volume
-docker volume inspect ${VOLUME_NAME}
 
 # done!
 echo ""
 echo "done."
-
